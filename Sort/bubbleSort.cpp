137aa92195b53dff0bff213d33a2a53a67d54a9b
bachbachkhoa <42577469+bachbachkhoa@users.noreply.github.com>
2019-11-22 21:48:10 +0700

    Update bubbleSort.cpp

--- Sort/bubbleSort.cpp
+++ Sort/bubbleSort.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
 
+using namespace std;
+
+
+void swap(int *a, int *b)
+{
+	int t;
+	t = *a;
+	*a = *b;
+	*b = t;
+}
+
+
+// Print output array
+void printArr(int a[], int n)
+{
+	for(int i = 0; i < n; ++i)
+	{
+		cout << " " << a[i];
+	}
+}
+
+void bubbleSort(int a[], int n)
+{
+	for(int i = 0; i < n - 1; ++i)
+	{
+		for(int j = n - 1; j >= i + 1; --j)
+		{
+			if (a[j] < a[j - 1])
+				swap(a[j], a[j - 1]);
+		}
+	}
+}
+
+
+int main()
+{
+	int a[] = {41, 23, 74, 11, 65, 58, 94, 36, 99, 87};
+	int n = sizeof(a)/sizeof(a[0]);
+
+	cout << "\nEntered array:";
+	printArr(a, n);
+	bubbleSort(a, n);
+	cout << "\nSorted array:";
+	printArr(a, n);
+	return 0;
+	return 0;
+}
