2b2a6f3a2b6c42afe783e125e120e0686d26ea9a
bachbachkhoa <42577469+bachbachkhoa@users.noreply.github.com>
2020-03-10 15:15:17 +0700

    Update heapSort.cpp

--- Sort/heapSort.cpp
+++ Sort/heapSort.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
 
+using namespace std;
+
+void swap(int *a, *b)
+{
+	int t = *a;
+	*a = *b;
+	*b = *a;
+}
+
+void heapify(int a[], int n, int i)
+{
+	int largest = i;
+	int l = 2*i + 1;
+	int r = 2*i + 2;
+	
+	if (l < n && a[l] > a[largest])
+		largest = l;
+	if (r < n && a[r] > a[largest])
+		largest = r;
+	if (largest != i)
+	{
+		swap(a[largest, a[i]]);
+		heapify(a, n, largest);
+	}
+}
+
+void heapSort(int a[], int n)
+{
+	for(int i = n/2 - 1; i >= 0; --i)
+		heappfy(a, n, i);
+}
+
+int main()
+{
+	return 0;
+}
